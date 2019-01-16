db64aa9a91c2a3b81c63c11571b5bbd2ffabebd9
McBk_Pro_Engine <dkria@dkrias-MacBook-Pro.local>
2019-01-16 13:14:26 -0800

    updates

--- server.js
+++ server.js
@@ -19,8 +19,7 @@ db.sequelize.sync({}).then(function() {
     app.listen(PORT, function() {
       console.log("App listening on PORT " + PORT);
     });
-  });
+  })
 
-module.exports = connection;
 
 
