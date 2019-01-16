0899f20fcb8bf5e7a33274d43df5064f5b26d286
McBk_Pro_Engine <dkria@dkrias-MacBook-Pro.local>
2019-01-16 12:39:01 -0800

    updates

--- server.js
+++ server.js
@@ -5,30 +5,6 @@ const mysql2 = require("mysql2");
 const sequelize = require("sequelize");
 const db = require("./models");
 
-var connection;
-if (process.env.JAWSDB_URL) {
-    // DB is JawsDB on Heroku
-    connection = mysql.createConnection(process.env.JAWSDB_URL);
-} else {
-    // DB is local on localhost
-    connection = mysql.createConnection({
-        port: 3306,
-        host: 'localhost',
-        user: 'root',
-        password: '',
-        database: 'office'
-    });
-}
-
-connection.connect(function (err) {
-    if (err) {
-        console.error('error connecting: ' + err.stack);
-        return;
-    }
-    console.log('connected as id ' + connection.threadId);
-});
-
-
 
 const PORT = process.env.PORT || 8080;
 app.use(express.static("public"))
